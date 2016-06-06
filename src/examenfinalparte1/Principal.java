0b0ffd529cd6301801e60604df265f7df601a3c7
propa <propa@DESKTOP-10ADTBO>
2016-06-06 09:04:22 +0200

    Formateado

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,22 @@
 package examenfinalparte1;
+
 public class Principal {
+
     int i;
-public Principal(int j, int m) {i=met(j,m);}
+
+    public Principal(int j, int m) {
+        i = met(j, m);
+    }
+
     public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+        i = met(j, j);
+    }
+
+    public Principal() {
+        i = -1;
+    }
+
+    public int met(int x, int x1) {
+        return x + x1;
+    }
+}
